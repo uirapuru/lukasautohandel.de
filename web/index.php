404df974bb1d3c618b3ae9c4873cd5e88ef57f38
Grzegorz Kaszuba <uirapuru@tlen.pl>
2013-09-08 03:08:40 +0200

    env up

--- web/index.php
+++ web/index.php
@@ -3,5 +3,5 @@ date_default_timezone_set('UTC');
 
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'dev', true);
+$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', true);
 sfContext::createInstance($configuration)->dispatch();
